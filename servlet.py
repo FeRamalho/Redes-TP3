a4519382a5b9533b5ebc205bb9c04f486bf685ed
FeRamalho <olliveira.fr@gmail.com>
2017-12-05 03:35:38 -0200

    topologia att

--- servlet.py
+++ servlet.py
@@ -23,7 +23,7 @@ def main():
 	for peer in peer_list:
 		peer_ip, x , peer_port = peer.partition(':')
 		connections.append( (peer_ip, int(peer_port)) )
-
+	
 	# Reading keys from input
 	input_file = open(input_name, 'r')
 	for line in input_file:
@@ -55,39 +55,27 @@ def main():
 			reqkey = data[6:].decode()
 			print(reqkey)
 			if(reqkey in keys.keys()):
-				print(keys[reqkey])
-				#answer = struct.pack('!H', 9) + struct.pack('!I', numsq) + keys[key].encode()
-				####################################################################
 				answer = struct.pack('!H', 9) + struct.pack('!I', numsq) + keys[reqkey].encode()
 				sock.sendto(answer, address)
 			# Not seen this req
 			if(address not in seen_req.keys() or [address] != numsq):
-				##print(address[0], address[0].encode())
 				ip4bytes = socket.inet_aton(address[0]) 
-# ERRO AQUI IP ORIGEM
-				##keyflood = struct.pack('!H', 7) + struct.pack('!H', 3) + struct.pack('!I', numsq) + address[0].encode() + struct.pack('!H', address[1]) + reqkey.encode()
 				keyflood = struct.pack('!H', 7) + struct.pack('!H', 3) + struct.pack('!I', numsq) + \
 				ip4bytes + struct.pack('!H', address[1]) + reqkey.encode()
 				seen_req[address] = numsq
 				for peer in connections:
 					if( peer != address ):
-						print("PEER: ",peer)
 						sock.sendto(keyflood, peer)
-
 
 		# TOPO REQ
 		elif(typ == 6):
-			print("TOPOREQ")
 			numsq = struct.unpack('!I', data[2:6])[0]
 			# Not seen this req
 			if(address not in seen_req.keys() or seen_req[address] != numsq):
-# ERRO AQUI IP ORIGEM
 				ip4bytes = socket.inet_aton(address[0])
-				#topoflood = struct.pack('!H', 8) + struct.pack('!H', 3) + struct.pack('!I', numsq) + address[0].encode() + struct.pack('!H', address[1])
 				#topoflood = struct.pack('!H', 8) + struct.pack('!H', 3) + struct.pack('!I', numsq) + ip4bytes + struct.pack('!H', address[1])
 				#topoflood com a info no final (ip:porto do servent)
 				ipsend = ":".join((ip_origem,str(port)))
-				print("IPSEND: ",ipsend)
 				topoflood = struct.pack('!H', 8) + struct.pack('!H', 3) + struct.pack('!I', numsq) + ip4bytes + struct.pack('!H', address[1]) + \
 				 ipsend.encode()
 				seen_req[address] = numsq
@@ -103,41 +91,37 @@ def main():
 		elif(typ == 7 or typ == 8):
 			ttl = struct.unpack('!H', data[2:4])[0]
 			numsq = struct.unpack('!I', data[4:8])[0]
-# ERRO AQUI IP ORIGEM
 			ip_origem = socket.inet_ntoa(data[8:12])
-			print('flood de ip origem = ', ip_origem)
+			#print('flood de ip origem = ', ip_origem)
 			port_origem = struct.unpack('!H', data[12:14])[0]
-			print('port origem = ', port_origem)
+			#print('port origem = ', port_origem)
 			origem = (ip_origem, port_origem)
 			# KEY FLOOD
 			if(typ == 7):
 				reqkey = data[14:].decode()
-				print(data)
 				if(reqkey in keys.keys()):
-					print(keys[reqkey])
-					#answer = struct.pack('!H', 9) + struct.pack('!I', numsq) + keys[key].encode()
-					####################################################################
-					answer = struct.pack('!H', 9) + struct.pack('!I', numsq) + keys[reqkey].encode()
-					sock.sendto(answer, origem)
+					if(origem not in seen_req.keys() or seen_req[origem] != numsq ): ##################################
+						seen_req[origem] = numsq #################################
+						answer = struct.pack('!H', 9) + struct.pack('!I', numsq) + keys[reqkey].encode()
+						sock.sendto(answer, origem)
 			# TOPO FLOOD
 			elif(typ == 8):
-				#answer = struct.pack('!H', 9) + struct.pack('!I', numsq) + data[6:] + my_address[0].encode() + struct.pack('!H', my_address[1])
-				#ip4bytes = socket.inet_aton(my_address[0])
-				ipsend = ":".join((ip_origem,str(my_address[1])))
-				print("IPSEND: ",ipsend)
-				ipsend = " " + ipsend
-				answer = struct.pack('!H', 9) + struct.pack('!I', numsq) + data[14:] + ipsend.encode()
-				sock.sendto(answer, origem)
+				if(origem not in seen_req.keys() or seen_req[origem] != numsq ): ##################################
+					seen_req[origem] = numsq #################################
+### VER SE O IP TA CERTO AQUI
+					ipsend = ":".join((ip_origem,str(my_address[1])))
+					ipsend = " " + ipsend
+					answer = struct.pack('!H', 9) + struct.pack('!I', numsq) + data[14:] + ipsend.encode()
+					sock.sendto(answer, origem)
 			# FLOOD
 			if(ttl > 0):
 				ttl = ttl -1
-# ERRO AQUI IP ORIGEM
 				ip4bytes = socket.inet_aton(origem[0])
-				#flood = struct.pack('!H', typ) + struct.pack('!H', ttl) + struct.pack('!I', numsq) + origem[0].encode() + struct.pack('!H', origem[1]) + data[14:]
 				flood = struct.pack('!H', typ) + struct.pack('!H', ttl) + struct.pack('!I', numsq) + \
 				ip4bytes + struct.pack('!H', origem[1]) + data[14:]
 				if(typ == 8):
 					#colocar um espaço e o meu ip no final como string ip:port
+### VER SE O IP TA CERTO AQUI
 					ipsend1 = ":".join((origem[0],str(port)))
 					ipsend1 = " " + ipsend1
 					flood += ipsend1.encode()
